e9c94690d0bb29db58449ac28597bbb82541841f
Caio Gonçalves <caiofgwf@gmail.com>
2021-11-18 16:58:38 -0300

    First Entry!

---
+++ Delphi Project/Unit2.pas
@@ -0,0 +1,28 @@
+unit Unit2;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  Data.DB, FireDAC.Comp.Client;
+
+type
+  TMainDM = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  MainDM: TMainDM;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
---
+++ Delphi Project/__history/Unit2.dfm.~1~
@@ -0,0 +1,5 @@
+object MainDM: TMainDM
+  OldCreateOrder = False
+  Height = 257
+  Width = 338
+end
